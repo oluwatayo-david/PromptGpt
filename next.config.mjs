7a53091c7ff2bfddbf9197589770cc3cdf9b2e30
David Oluwatayo <oluwatayodavid273@gmail.com>
2024-10-12 12:12:34 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  headers: () => [
+    {
+      // Apply this header to all API routes
+      source: '/api/(.*)',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+        },
+      ],
+    },
+  ],
+
   experimental: {
     serverComponentsExternalPackages: ["mongoose"], // Keep this if you are using mongoose
   },
 
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: ["lh3.googleusercontent.com"], // External image domains
   },
 
   webpack(config) {
     config.experiments = {
       ...config.experiments,
-      topLevelAwait: true,
+      topLevelAwait: true, // Enabling top-level await in Webpack
     };
     return config;
   },

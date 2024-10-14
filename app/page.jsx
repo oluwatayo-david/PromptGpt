b003060da4b706094b61109ae19ab75e4804e056
David Oluwatayo <oluwatayodavid273@gmail.com>
2024-10-14 20:18:42 +0100

    update

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ const Home = () => (
       <span className='text-center orange_gradient'> AI-Powered Prompts</span>
     </h1>
     <p className='text-center desc'>
-      Promptopia is an open-source AI prompting tool for modern world to
+      PromptGpt is an open-source AI prompting tool for modern world to
       discover, create and share creative prompts
     </p>
 
